ca2101cca89830b1da930bc410e3bfa1ec270000
ZUBER Lionel <lionel.zuber@niji.fr>
2022-12-12 09:38:24 +0100

    Initialize ngxs repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { CoreModule } from 'src/core/core.module';
+import { environment } from 'src/environments/environment';
 import { ShareModule } from 'src/share/share.module';
 import { TodosModule } from 'src/todos/todos.module';
-import { CoreModule } from 'src/core/core.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ShareModule, TodosModule, CoreModule],
+  imports: [
+    ShareModule,
+    TodosModule,
+    CoreModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    ...environment.plugins,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
